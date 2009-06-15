c1a32f0baa5578826b9e08019b735cd278531518
Fabien Potencier <fabien.potencier@gmail.com>
2009-06-15 18:20:23 +0200

    added PHPDoc to the Pimple class

--- lib/Pimple.php
+++ lib/Pimple.php
@@ -1,24 +1,77 @@
 <?php
 
+/*
+ * This file is part of Pimple.
+ *
+ * Copyright (c) 2009 Fabien Potencier
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is furnished
+ * to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+/**
+ * Pimple main class.
+ *
+ * @package pimple
+ * @author  Fabien Potencier
+ */
 class Pimple
 {
   protected $values = array();
 
+  /**
+   * Sets a parameter or an object.
+   *
+   * @param string $id    The unique identifier for the parameter or object
+   * @param mixed  $value The value of the parameter or a closure to defined an object
+   */
   function __set($id, $value)
   {
     $this->values[$id] = $value;
   }
 
+  /**
+   * Gets a parameter or an object.
+   *
+   * @param  string $id The unique identifier for the parameter or object
+   *
+   * @return mixed  The value of the parameter or an object
+   *
+   * @throws InvalidArgumentException if the identifier is not defined
+   */
   function __get($id)
   {
     if (!isset($this->values[$id]))
     {
-      throw new InvalidArgumentException(sprintf('Value "%s" is not defined.', $id));
+      throw new InvalidArgumentException(sprintf('Identifier "%s" is not defined.', $id));
     }
 
     return is_callable($this->values[$id]) ? $this->values[$id]($this) : $this->values[$id];
   }
 
+  /**
+   * Returns a closure that stores the result of the given closure for
+   * uniqueness in the scope of this instance of Pimple.
+   *
+   * @param Closure $callable A closure to wrap for uniqueness
+   *
+   * @return Closure The wrapped closure
+   */
   function asShared($callable)
   {
     return function ($c) use ($callable)
