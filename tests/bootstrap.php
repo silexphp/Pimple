73834794062fe13f06241d0d072baaa9abc7107c
Dominik Zogg <dominik.zogg@gmail.com>
2014-04-28 13:13:32 +0200

    use Pimple.php for class alias

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -10,8 +10,8 @@
  */
 
 require_once __DIR__.'/../lib/Pimple/Container.php';
+require_once __DIR__.'/../lib/Pimple.php';
 require_once __DIR__. '/../lib/Pimple/ServiceProviderInterface.php';
-require_once __DIR__.'/../lib/class.aliases.php';
 require_once __DIR__.'/Pimple/Tests/Service.php';
 require_once __DIR__.'/Pimple/Tests/Invokable.php';
 require_once __DIR__.'/Pimple/Tests/NonInvokable.php';
