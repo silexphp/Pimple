c068bf5af3e66255bf30bd6e037dd094c76e838a
Pascal Luna <skalpa@zetareticuli.org>
2017-07-11 13:20:59 +0100

    Bumped version to 3.2.0

--- ext/pimple/php_pimple.h
+++ ext/pimple/php_pimple.h
@@ -41,7 +41,7 @@ extern zend_module_entry pimple_module_entry;
 #include "TSRM.h"
 #endif
 
-#define PIMPLE_VERSION "3.1.0-DEV"
+#define PIMPLE_VERSION "3.2.0-DEV"
 
 #define PIMPLE_NS "Pimple"
 #define PSR_CONTAINER_NS "Psr\\Container"
