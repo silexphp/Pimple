fb777b9bd8b1af0390b02ef45f6f5504653c340d
Derek Stephen McLean <delboy1978uk@gmail.com>
2020-03-08 03:07:37 +0100

    remove entity methods

--- src/RegistrationInterface.php
+++ src/RegistrationInterface.php
@@ -10,14 +10,4 @@ interface RegistrationInterface
      * @param Container $c
      */
     public function addToContainer(Container $c);
-
-    /**
-     * @return string
-     */
-    function getEntityPath(): string;
-
-    /**
-     * @return bool
-     */
-    function hasEntityPath(): bool;
 }
