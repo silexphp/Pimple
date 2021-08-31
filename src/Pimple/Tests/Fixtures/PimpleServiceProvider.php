2d492d655d0eefbb2bf395e8e271b473e774aa14
Greg ORIOL <greg@gregoriol.net>
2021-08-31 15:58:39 +0200

    Fixes typo

--- src/Pimple/Tests/Fixtures/PimpleServiceProvider.php
+++ src/Pimple/Tests/Fixtures/PimpleServiceProvider.php
@@ -37,7 +37,7 @@ class PimpleServiceProvider implements ServiceProviderInterface
      * This method should only be used to configure services and parameters.
      * It should not get services.
      *
-     * @param Container $pimple An Container instance
+     * @param Container $pimple A Container instance
      */
     public function register(Container $pimple)
     {
